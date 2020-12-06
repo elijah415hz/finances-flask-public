612236ff9eb7bc8397d6884a96c9f667ec6f0afa
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-12-06 10:07:38 -0800

    removed create_app function

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,21 +1,19 @@
 from flask import Flask
 from sqlalchemy import create_engine
 import os
+from . import auth, expenses, income, datalists, views
 FLASK_DB_URI = os.environ.get("FLASK_DB_URI")
 
 # Create database connection
 engine = create_engine(FLASK_DB_URI) 
 
-def create_app():
-    app = Flask(__name__, static_folder='../build', static_url_path="/")
-    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    app.config['SECRET_KEY'] = os.urandom(5)
+# Setup app
+app = Flask(__name__, static_folder='../build', static_url_path="/")
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SECRET_KEY'] = os.urandom(5)
+app.register_blueprint(auth.bp)
+app.register_blueprint(expenses.bp)
+app.register_blueprint(income.bp)
+app.register_blueprint(datalists.bp)
+app.register_blueprint(views.bp)
 
-    from . import auth, expenses, income, datalists, views
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(expenses.bp)
-    app.register_blueprint(income.bp)
-    app.register_blueprint(datalists.bp)
-    app.register_blueprint(views.bp)
-
-    return app

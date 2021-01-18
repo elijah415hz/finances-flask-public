1266c44be158918e70b339ac8c7b0d80e23a175e
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2021-01-18 15:04:20 -0800

    Restyled login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,28 +2,50 @@ import React, { useState } from 'react'
 import { Redirect } from 'react-router-dom'
 import API from '../utils/API'
 import { AuthContext } from '../App'
-import { Button, TextField, Dialog, Container } from '@material-ui/core';
+import { Button, TextField, Dialog, Container, Typography } from '@material-ui/core';
+import Grid, { GridSpacing } from '@material-ui/core/Grid';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 import Signup from '../components/Signup'
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
+            flexGrow: 1
+        },
+        heading: {
+            textAlign: 'center',
+            fontFamily: 'Times New Roman, serif'
+        },
+        signupBtn: {
+            float: 'right',
+            margin: '1em',
+        },
+        sidebar: {
+            display: 'flex',
+            justifyContent: 'center',
+            textAlign: 'center',
+            flexWrap: 'wrap',
+        },
+        form: {
             display: 'flex',
             justifyContent: 'center',
             textAlign: 'center',
             flexWrap: 'wrap',
             '& > *': {
                 margin: theme.spacing(1),
+                display: 'flex',
+                justifyContent: 'center',
+                textAlign: 'center',
+                flexWrap: 'wrap',
                 [theme.breakpoints.down('xs')]: {
                     width: '100%',
                 },
             }
         },
-        signupBtn: {
-            float: 'right',
-            margin: '1em',
-        },
+        link: {
+            color: theme.palette.primary.main,
+            '&:hover': theme.palette.secondary.main 
+        }
     })
 );
 
@@ -75,36 +97,43 @@ export default function Login() {
 
     return (
         <>
-            <Button
-                className={classes.signupBtn}
-                variant="contained"
-                color="primary"
+            <Grid container className={classes.root}>
+                <Grid item xs={12}>
+                    <Typography variant="h1" component="h2" gutterBottom className={classes.heading}>
+                        Flask of Finances
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} className={classes.sidebar}>
+                    <form className={classes.form} onSubmit={formSubmit}>
+                        <TextField
+                            onChange={inputChange}
+                            value={loginFormState.username}
+                            type="text"
+                            name="username"
+                            label="username"
+                            variant="outlined"
+                        />
+                        <TextField
+                            onChange={inputChange}
+                            value={loginFormState.password}
+                            type="password"
+                            name="password"
+                            label="password"
+                            variant="outlined"
+                        />
+                        <Button variant="contained" color="primary" name="login" type="submit">Login</Button>
+                            </form>
+                            </Grid>
+                        <Grid item xs={12} className={classes.sidebar}>
+
+            <a href="#"
+                className={classes.link}
                 onClick={() => setSignupOpen(true)}
-            >Sign Up</Button>
-            <Container className={classes.root}>
-                <h4>Login</h4>
-                    </Container>
-                <form className={classes.root} onSubmit={formSubmit}>
-                    <TextField
-                        onChange={inputChange}
-                        value={loginFormState.username}
-                        type="text"
-                        name="username"
-                        label="username"
-                        variant="outlined"
-                    />
-                    <TextField
-                        onChange={inputChange}
-                        value={loginFormState.password}
-                        type="password"
-                        name="password"
-                        label="password"
-                        variant="outlined"
-                    />
-                    <Button variant="contained" color="primary" name="login" type="submit">Login</Button>
-                </form>
+            >Sign Up</a>
+            </Grid>
+            </Grid>
             <Dialog onClose={handleClose} open={signupOpen} maxWidth='xl'>
-                <Signup handleClose={handleClose}/>
+                <Signup handleClose={handleClose} />
             </Dialog>
         </>
     );
